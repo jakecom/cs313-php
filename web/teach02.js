3b744804755e1915965ac3b4fbe70e0562c3e8cc
Jake Hedgecock <hed15002@byui.edu>
2017-09-18 19:37:13 -0700

    more edits

--- web/teach02.js
+++ web/teach02.js
@@ -9,6 +9,6 @@ function colorChange(){
 	var div_id = "div1";
 	var div = document.getElementById(div_id);
 
-	var color = textbox.value;
-	div.style.backgroundColor = color;
+	var colors = textbox.value;
+	div.style.backgroundColor = colors;
 }

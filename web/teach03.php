0534ac818cd5171a845d776bd3185434b945cabb
Jake Hedgecock <hed15002@byui.edu>
2017-09-24 19:04:01 -0700

    started teach assignment

--- web/teach03.php
+++ web/teach03.php
@@ -1,10 +1,22 @@
+<?php
+
+$name = htmlspecialchars($_POST["name"]);
+$email = htmlspecialchars($_POST["email"]);
+$major = htmlspecialchars($_POST["major"]);
+$comments = htmlspecialchars($_POST["comments"]);
+
+
+?>
+
+<!DOCTYPE html>
 <html>
 <body>
 
-Welcome <?php echo $_POST["name"]; ?><br>
-Your email address is: <?php echo $_POST["email"]; ?><br>
-Your Major is: <?php echo$_POST["major"]; ?><br>
-These are your comments: <?php echo $_POST["comments"]; ?><br>
+<p>Welcome <?=$name ?></p><br>
+<p>Your email address is: <?=$email ?></p><br>
+<p>Your Major is: <?=$major ?></p><br>
+<p>These are your comments: <?=$comments ?></p><br>
+<p>These are the countries you've visited:</p>
 
 
 </body>

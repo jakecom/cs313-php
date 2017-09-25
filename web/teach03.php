4959c3e59f685cdcdf51307017a00091ce8e7abf
Jake Hedgecock <hed15002@byui.edu>
2017-09-24 18:50:01 -0700

    started teach assignment

---
+++ web/teach03.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+
+Welcome <?php echo $_POST["name"]; ?><br>
+Your email address is: <?php echo $_POST["email"]; ?><br>
+Your Major is: <?php echo$_POST["major"]; ?><br>
+These are your comments: <?php echo $_POST["comments"]; ?><br>
+
+
+</body>
+</html> 
